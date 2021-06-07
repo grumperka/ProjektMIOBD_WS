e42f93f162ef1b15a10a12e1e9920e1a36af5eee
grumperka <karolina.siwiak@vp.pl>
2021-06-07 21:40:47 +0200

    Rezerwacja ulepszona

--- ProjectMongoDBReact/Services/RezerwacjaService.cs
+++ ProjectMongoDBReact/Services/RezerwacjaService.cs
@@ -29,25 +29,55 @@ namespace ProjectMongoDBReact.Services
 
         public Rezerwacja Create(Rezerwacja r)
         {
-            var klient = _klient.Find<Klient>(f => f.email == r.id_rezerwujacego).FirstOrDefault();
-
-            if (klient == null) {
-                _klient.InsertOne(new Klient { email = r.id_rezerwujacego, imie = "Joe/Jane", nazwisko = "Doe", nr_tel = 000000000 });
-            }
-
-            klient = _klient.Find<Klient>(f => f.email == r.id_rezerwujacego).FirstOrDefault();
+            Klient klient = KlientCheck(r.id_rezerwujacego);
 
             var pokoj = _pokoj.Find<Pokoj>(f => f.Id == r.id_pokoju).FirstOrDefault();
 
             if (pokoj == null) { return null; }
 
-            String diff = (r.koniec - r.poczatek).TotalDays.ToString();
+            bool zajety = czyZajety(pokoj, r.poczatek, r.koniec);
 
-            int ileDni = Int32.Parse(diff);
+            if (zajety == false)
+            {
+                String diff = (r.koniec - r.poczatek).TotalDays.ToString();
 
-            Rezerwacja nowa = new Rezerwacja { id_klienta = klient.Id, id_rezerwujacego = klient.Id, id_pokoju = r.id_pokoju, poczatek = r.poczatek, koniec = r.koniec, koszt = r.koszt * ileDni, dataEdycji = r.dataEdycji, czyAnulowana = r.czyAnulowana, czyEdytowana = r.czyEdytowana };
-            _rezerwacja.InsertOne(nowa);
-            return nowa;
+                int ileDni = Int32.Parse(diff);
+
+                Rezerwacja nowa = new Rezerwacja { id_klienta = klient.Id, id_rezerwujacego = klient.Id, id_pokoju = r.id_pokoju, poczatek = r.poczatek, koniec = r.koniec, koszt = r.koszt * ileDni, dataEdycji = r.dataEdycji, czyAnulowana = r.czyAnulowana, czyEdytowana = r.czyEdytowana };
+                _rezerwacja.InsertOne(nowa);
+                return nowa;
+            }
+            else 
+                return null;
+        }
+
+        public bool czyZajety(Pokoj p, DateTime begin, DateTime end) {
+
+            var result0 = _rezerwacja.Find<Rezerwacja>(f => f.id_pokoju == p.Id &&
+            f.czyAnulowana == false &&
+            ((begin >= f.poczatek && begin <= f.koniec) ||
+            (end >= f.poczatek && end <= f.koniec))).Any();
+
+            var result1 = _rezerwacja.Find<Rezerwacja>(f => f.id_pokoju == p.Id &&
+            f.czyAnulowana == false &&
+            begin < f.poczatek &&
+            end >= f.koniec).Any();
+
+            if (result0 == false && result1 == false) { return false; }
+            else 
+                return true;
+        }
+
+        public Klient KlientCheck(string email) {
+            var klient = _klient.Find<Klient>(f => f.email == email).FirstOrDefault();
+
+            if (klient == null)
+            {
+                _klient.InsertOne(new Klient { email = email, imie = "Joe/Jane", nazwisko = "Doe", nr_tel = 000000000 });
+            }
+
+            klient = _klient.Find<Klient>(f => f.email == email).FirstOrDefault();
+            return klient;
         }
     }
 }
